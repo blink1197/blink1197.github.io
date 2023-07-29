ecc0a970eeaf5c4b5ac4b9a03c7d3f1cf1ab6e3b
blink1197 <cgbagotsay11@gmail.com>
2023-07-29 22:40:37 +0800

    Added first page

--- main.js
+++ main.js
@@ -15,5 +15,40 @@ $(document).ready(function() {
         
     });
 
+    function updateCountdown() {
+        // Set the target date (October 23, 2023 1:00 PM GMT+8)
+        const targetDate = new Date("2023-10-23T13:00:00+08:00");
+  
+        // Get the current date and time
+        const now = new Date();
+  
+        // Calculate the time difference between now and the target date
+        const timeDifference = targetDate - now;
+  
+        // Calculate the number of days, hours, minutes, and seconds
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+        // Format the countdown string
+        const countdownStringDays = `${days.toString().padStart(2, "0")}`;
+        const countdownStringHours = `${hours.toString().padStart(2, "0")}`;
+        const countdownStringMinutes = `${minutes.toString().padStart(2, "0")}`;
+        const countdownStringSeconds = `${seconds.toString().padStart(2, "0")}`;
+  
+        // Display the countdown on the HTML element
+        $("#days").text(countdownStringDays);
+        $("#hours").text(countdownStringHours);
+        $("#minutes").text(countdownStringMinutes);
+        $("#seconds").text(countdownStringSeconds);
+      }
+  
+      // Update the countdown every second
+      setInterval(updateCountdown, 1000);
+  
+      // Call the updateCountdown function immediately to display the initial countdown
+      updateCountdown();
+
     
 });
